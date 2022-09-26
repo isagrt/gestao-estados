f32ee9680b5678e450ade08d17aad76ba041ff60
isagrt <isadoragoedert05@gmail.com>
2022-09-26 09:06:36 -0300

    mDs eduardo pague minha terapia

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,25 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+import { count } from "console";
+import { mapStores, mapState, mapActions } from "pinia";
+export default {
+  data() {
+    return {
+      nome: "Isadora",
+      nome_editar: "Isadora",
+    };
+  },
+  computed: {
+    ...mapStores(useCounterStore),
+    ...mapActions(useCounterStore), (count, doubleCount)
+  },
+};
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Pagina Main</h1>
+    <input type="text" v-model="nome_editar" />
+    <button @click="nome = nome_editar">Enviar</button>
+    <h2>{{ nome }}</h2>
   </main>
 </template>
